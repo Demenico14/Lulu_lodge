6266cff701932c6b3bd6b8ca27c5151d5949b60d
Dominic Mushayi <mushayidominic@gmail.com>
2024-09-02 20:23:22 +0200

    forth

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,6 +1,6 @@
 export const images = [
-  { src: '/assets/images/picture5.jpg', alt: 'First' },
-  { src: '/assets/images/picture3.jpg', alt: 'Second' },
-  { src: '/assets/images/picture8.jpg', alt: 'Third' },
+  { src: '/assets/images/picture5.webp', alt: 'First' },
+  { src: '/assets/images/picture3.webp', alt: 'Second' },
+  { src: '/assets/images/picture8.webp', alt: 'Third' },
   { src: '/assets/images/Victoria.jpg', alt: 'Fourth' }
 ]
