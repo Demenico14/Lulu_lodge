9cdb7c6464323a58b52e2ec69795f973bd036189
Dominic Mushayi <mushayidominic@gmail.com>
2024-09-05 05:34:00 +0200

    update

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -6,17 +6,12 @@ import HeroSection from '@/components/HeroSection/HeroSection';
 import PageSearch from '@/components/PageSearch/PageSearch';
 import FeaturedRoom from '@/components/FeaturedRoom/FeaturedRoom';
 import OfferCard from '@/components/OfferCard/OfferCard';
-import { getFeaturedRoom } from '@/libs/apis';
 import NewsLetter from '@/components/NewsLetter/NewsLetter';
 
 
 
 
-const Home = async () => {
-  const featuredRoom = await getFeaturedRoom();
-
-  console.log(featuredRoom)
-
+const Home =  () => {
 
   return (
     <>
@@ -24,7 +19,7 @@ const Home = async () => {
       <HeroSection/>
       <Counter/>
       <PageSearch />
-      <FeaturedRoom featuredRoom={featuredRoom} />
+      <FeaturedRoom  />
       <OfferCard/>
       <NewsLetter/>
     </>
--- src/components/FeaturedRoom/FeaturedRoom.tsx
+++ src/components/FeaturedRoom/FeaturedRoom.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image"; // Import the Image component
-import { Room } from "@/models/room";
+
 import Link from "next/link";
 
 
