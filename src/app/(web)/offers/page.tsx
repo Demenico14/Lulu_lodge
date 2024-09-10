41110fe3807bd98fb7ac068e18b99698dcc0b840
Dominic Mushayi <mushayidominic@gmail.com>
2024-09-10 13:31:09 +0200

    final build

--- src/app/(web)/offers/page.tsx
+++ src/app/(web)/offers/page.tsx
@@ -45,8 +45,8 @@ const Offers = () => {
           </p>
           <div className="grid grid-cols-2 gap-4">
             {[
-              "/assets/images/picture5.webp",
-              "/assets/images/picture5.webp",
+              "/assets/images/eli.webp",
+              "/assets/images/rhino.webp",
             ].map((src, index) => (
               <Image
                 key={index}
@@ -72,41 +72,13 @@ const Offers = () => {
           </p>
           <div className="grid grid-cols-2 gap-4">
             {[
-              "/assets/images/picture5.webp",
-              "/assets/images/picture5.webp",
+              "/assets/images/pickup.jpg",
+              "/assets/images/carpickup2.webp",
             ].map((src, index) => (
               <Image
                 key={index}
                 src={src}
                 alt={`design template ${index + 1}`}
-                width={500}
-                height={500}
-                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-              />
-            ))}
-          </div>
-        </div>
-      ),
-    },
-    {
-      title: "Taxi-Rentals",
-      content: (
-        <div>
-          <p className="text-dark-400 dark:text-white mb-3">
-            For flexibility and independence, the lodge offers taxi rentals.
-            Guests can rent a taxi with or without a driver for easy
-            transportation around town or to visit popular attractions at their
-            own pace.
-          </p>
-          <div className="grid grid-cols-2 gap-4">
-            {[
-              "/assets/images/picture5.webp",
-              "/assets/images/picture5.webp",
-            ].map((src, index) => (
-              <Image
-                key={index}
-                src={src}
-                alt={`startup template ${index + 1}`}
                 width={500}
                 height={500}
                 className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
