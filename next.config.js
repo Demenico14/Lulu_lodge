005c2a12807f7907acb27ec26690f7148962dd1c
Dominic Mushayi <mushayidominic@gmail.com>
2024-09-27 22:13:34 +0200

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', "cdn.sanity.io"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
