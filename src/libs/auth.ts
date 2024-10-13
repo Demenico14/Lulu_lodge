5ad87f1375978a1ffbabfaf02d7ea28efca9f797
Dominic Mushayi <mushayidominic@gmail.com>
2024-10-13 18:00:03 +0200

    Update auth.ts

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,16 +1,11 @@
 import { NextAuthOptions } from 'next-auth';
 import { SanityAdapter, SanityCredentials } from 'next-auth-sanity';
-import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 
 import sanityClient from './sanity';
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID as string,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
